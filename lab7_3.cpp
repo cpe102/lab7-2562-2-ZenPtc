6289d7e07b854a4257b9473840840c7ea5ee0b96
ZenPtc <maku4fb@gmail.com>
2020-01-21 15:34:48 +0700

    lab 7.3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,32 @@
 //status = "ONE-NIGHT-STAND";
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+    float money,height;
+    string status;
+
+    cin >> age ;
+    if(age<=20){
+        cin >> height;
+        if(height < 160) status = "UNFRIEND";
+        else if(height < 175) status = "FRIEND";
+        else{
+            cin >> money ;
+            if(money>69000000) status = "MARRIED";
+            else status = "ONE-NIGHT-STAND";
+        }
+    }else if(age<30){
+        cin >> money;
+        if(money > 10000000) status = "BEST FRIEND";
+        else status = "UNFRIEND";
+    }else{
+        status = "UNFRIEND";
+    }
+
+    cout << status;
+
+    return 0;
+}
